7709b22c22d9868d6e4641552ce8689dbbd7cf3d
Victor Lazaro <lazarofilm@gmail.com>
2020-12-30 18:04:12 -0500

    Started working on translation logic

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,36 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+const Hidden = require("util");
+
+const hidden = (...message) =>
+  Hidden.inspect(...message, { showHidden: true, depth: null });
 
 class Translator {
+  translate(text, locale) {
+    let translated = "";
+    const splitText = text.split(" ");
+    console.log(splitText);
+    splitText.forEach((aWord) => {
+      // checks for uppercase
 
+      const bWord = aWord.match(/^[A-Z]/)
+        ? this.capitalize(americanToBritishSpelling[aWord.toLowerCase()])
+        : americanToBritishSpelling[aWord];
+
+      translated += (bWord || aWord) + " ";
+    });
+    translated = translated.trim();
+    console.log("text:      ", hidden(text));
+    console.log("translated:", hidden(translated));
+    return translated;
+    // Look for Caps, carriage returns (/n)
+  }
+
+  capitalize(word) {
+    return word && word[0].toUpperCase() + word.slice(1);
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,20 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    console.log("_____POST/translate_____");
+    console.log(req.body);
+    // req.body => {text, locale}
+    const text = req.body.text;
+    const locale = req.body.locale;
+    const translation = translator.translate(text, locale);
+    res.json({
+      text,
+      translation,
     });
+  });
 };
