5911ae5c6c4b2b9fcdda8bffc81d8609990b38cc
Victor Lazaro <lazarofilm@gmail.com>
2020-12-31 17:52:56 -0500

    reworked tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,60 +4,51 @@ const assert = chai.assert;
 const Translator = require("../components/translator.js");
 const translator = new Translator();
 
-suite("Unit Tests", () => {
-  suite("to British English", () => {
-    test("Translate Mangoes are my favorite fruit.", () => {
+suite(`Unit Tests`, () => {
+  suite(`to British English`, () => {
+    test(`Translate Mangoes are my favorite fruit.`, () => {
       assert.equal(
-        translator.translate(
-          "Mangoes are my favorite fruit.",
-          "american-to-british"
-        ),
-        "Mangoes are my favourite fruit."
+        translator.ab(`Mangoes are my favorite fruit.`, `american-to-british`),
+        `Mangoes are my <span class="highlight">favourite</span> fruit.`
       );
     });
-    test("I ate yogurt for breakfast.", () => {
+    test(`I ate yogurt for breakfast.`, () => {
       assert.equal(
-        translator.translate(
-          "I ate yogurt for breakfast.",
-          "american-to-british"
-        ),
-        "I ate yoghurt for breakfast."
+        translator.ab(`I ate yogurt for breakfast.`, `american-to-british`),
+        `I ate <span class="highlight">yoghurt</span> for breakfast.`
       );
     });
-    test("We had a party at my friend's condo.", () => {
+    test(`We had a party at my friend's condo.`, () => {
       assert.equal(
-        translator.translate(
-          "We had a party at my friend's condo.",
-          "american-to-british"
+        translator.ab(
+          `We had a party at my friend's condo.`,
+          `american-to-british`
         ),
-        "We had a party at my friend's flat."
+        `We had a party at my friend's <span class="highlight">flat</span>.`
       );
     });
-    test("Can you toss this in the trashcan for me?", () => {
+    test(`Can you toss this in the trashcan for me?`, () => {
       assert.equal(
-        translator.translate(
-          "Can you toss this in the trashcan for me?",
-          "american-to-british"
+        translator.ab(
+          `Can you toss this in the trashcan for me?`,
+          `american-to-british`
         ),
-        "Can you toss this in the bin for me?"
+        `Can you toss this in the <span class="highlight">bin</span> for me?`
       );
     });
-    test("The parking lot was full.", () => {
+    test(`The parking lot was full.`, () => {
       assert.equal(
-        translator.translate(
-          "The parking lot was full.",
-          "american-to-british"
-        ),
-        "The car park was full."
+        translator.ab(`The parking lot was full.`, `american-to-british`),
+        `The <span class="highlight">car park</span> was full.`
       );
     });
-    test("Like a high tech Rube Goldberg machine.", () => {
+    test(`Like a high tech Rube Goldberg machine.`, () => {
       assert.equal(
-        translator.translate(
-          "Like a high tech Rube Goldberg machine.",
-          "american-to-british"
+        translator.ab(
+          `Like a high tech Rube Goldberg machine.`,
+          `american-to-british`
         ),
-        "Like a high tech Heath Robinson device."
+        `Like a high tech <span class="highlight">Heath Robinson device</span>.`
       );
     });
   });
