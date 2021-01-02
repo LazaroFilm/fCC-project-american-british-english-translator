b5d4613b7b12037368b35ef69df71433e0baf933
Victor Lazaro <lazarofilm@gmail.com>
2021-01-02 16:48:11 -0500

    added time and refactored trnasaltion

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,8 @@ const americanOnly = require("./american-only.js");
 const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
+const britishToAmericanSpelling = _.invert(americanToBritishSpelling);
+const britishToAmericanTitles = _.invert(americanToBritishTitles);
 
 class Translator {
   ab(text) {
@@ -10,28 +12,52 @@ class Translator {
     translated = this.translate(translated, americanOnly);
     translated = this.translate(translated, americanToBritishSpelling);
     translated = this.translate(translated, americanToBritishTitles);
+    translated = translated.replace(
+      /(\d{1,2})(:)(\d\d)/g,
+      (_, hours, separator, minutes) => {
+        return `<span class="highlight">${hours}.${minutes}</span>`;
+      }
+    );
+    // console.log(translated);
     return translated;
   }
 
   ba(text) {
     let translated = text;
     translated = this.translate(translated, britishOnly);
-    translated = this.translate(
-      translated,
-      _.invert(americanToBritishSpelling)
+    translated = this.translate(translated, britishToAmericanSpelling);
+    translated = this.translate(translated, britishToAmericanTitles);
+    translated = translated.replace(
+      /(\d{1,2})(\.)(\d\d)/g,
+      (_, hours, separator, minutes) => {
+        return `<span class="highlight">${hours}:${minutes}</span>`;
+      }
     );
-    translated = this.translate(translated, _.invert(americanToBritishTitles));
+    // console.log(translated);
     return translated;
   }
 
   translate(translated, list) {
-    for (const word in list) {
+    // orders the list form longest to shortest word
+    const ordered = Object.keys(list).sort((a, b) => {
+      return b.length - a.length;
+    });
+    for (const word of ordered) {
       const bWord = list[word];
-      const re = new RegExp(`(${word})(\\W)`, "ig");
-      translated = translated.replace(re, (_, a, b) => {
-        return a === this.capitalize(word)
-          ? `<span class="highlight">${this.capitalize(bWord)}</span>${b || ""}`
-          : `<span class="highlight">${bWord}</span>${b || ""}`;
+      const re = new RegExp(word, "ig");
+      // looks for word and replaces it
+      translated = translated.replace(re, (match, index) => {
+        console.log(match[0]);
+        // checks if already translated
+        if (
+          translated.substr(index - 24, index) === `<span class="highlight">`
+        ) {
+          return match;
+        } else {
+          return match[0] === this.capitalize(word[0])
+            ? `<span class="highlight">${this.capitalize(bWord)}</span>`
+            : `<span class="highlight">${bWord}</span>`;
+        }
       });
     }
     return translated;
