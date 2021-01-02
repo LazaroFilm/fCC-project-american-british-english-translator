a89fd1be74a43378cdd8524e8ae69af022f9f581
Victor Lazaro <lazarofilm@gmail.com>
2021-01-02 16:48:59 -0500

    unit tests done

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -9,25 +9,19 @@ suite(`Unit Tests`, () => {
   suite(`Translate to British English`, () => {
     test(`Translate Mangoes are my favorite fruit.`, () => {
       assert.equal(
-        translator
-          .ab(`Mangoes are my favorite fruit.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`Mangoes are my favorite fruit.`).replace(re, ""),
         `Mangoes are my favourite fruit.`
       );
     });
     test(`I ate yogurt for breakfast.`, () => {
       assert.equal(
-        translator
-          .ab(`I ate yogurt for breakfast.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`I ate yogurt for breakfast.`).replace(re, ""),
         `I ate yoghurt for breakfast.`
       );
     });
     test(`We had a party at my friend's condo.`, () => {
       assert.equal(
-        translator
-          .ab(`We had a party at my friend's condo.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`We had a party at my friend's condo.`).replace(re, ""),
         `We had a party at my friend's flat.`
       );
     });
@@ -44,16 +38,14 @@ suite(`Unit Tests`, () => {
     });
     test(`The parking lot was full.`, () => {
       assert.equal(
-        translator
-          .ab(`The parking lot was full.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`The parking lot was full.`).replace(re, ""),
         `The car park was full.`
       );
     });
     test(`Like a high tech Rube Goldberg machine.`, () => {
       assert.equal(
         translator
-          .ab(`Like a high tech Rube Goldberg machine.`, `american-to-british`)
+          .ab(`Like a high tech Rube Goldberg machine.`)
           .replace(re, ""),
         `Like a high tech Heath Robinson device.`
       );
@@ -71,26 +63,20 @@ suite(`Unit Tests`, () => {
     });
     test(`No Mr. Bond, I expect you to die.`, () => {
       assert.equal(
-        translator
-          .ab(`No Mr. Bond, I expect you to die.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`No Mr. Bond, I expect you to die.`).replace(re, ""),
         `No Mr Bond, I expect you to die.`
       );
     });
     test(`Dr. Grosh will see you now.`, () => {
       assert.equal(
-        translator
-          .ab(`Dr. Grosh will see you now.`, `american-to-british`)
-          .replace(re, ""),
+        translator.ab(`Dr. Grosh will see you now.`).replace(re, ""),
         `Dr Grosh will see you now.`
       );
     });
     test(`Lunch is at 12:15 today.`, () => {
       assert.equal(
-        translator
-          .ab(`Lunch is at 12:15 today.`, `american-to-british`)
-          .replace(re, ""),
-        `Lunch is at 12.15`
+        translator.ab(`Lunch is at 12:15 today.`).replace(re, ""),
+        `Lunch is at 12.15 today.`
       );
     });
   });
@@ -98,87 +84,105 @@ suite(`Unit Tests`, () => {
     test(`We watched the footie match for a while.`, () => {
       assert.equal(
         translator
-          .ab(`We watched the footie match for a while.`, `british-to-american`)
+          .ba(`We watched the footie match for a while.`)
           .replace(re, ""),
-        ``
+        `We watched the soccer match for a while.`
       );
     });
     test(`Paracetamol takes up to an hour to work.`, () => {
       assert.equal(
         translator
-          .ab(`Paracetamol takes up to an hour to work.`, `british-to-american`)
+          .ba(`Paracetamol takes up to an hour to work.`)
           .replace(re, ""),
-        ``
+        `Tylenol takes up to an hour to work.`
       );
     });
     test(`First, caramelise the onions.`, () => {
       assert.equal(
-        translator
-          .ab(`First, caramelise the onions.`, `british-to-american`)
-          .replace(re, ""),
-        ``
+        translator.ba(`First, caramelise the onions.`).replace(re, ""),
+        `First, caramelize the onions.`
       );
     });
     test(`I spent the bank holiday at the funfair.`, () => {
       assert.equal(
         translator
-          .ab(`I spent the bank holiday at the funfair.`, `british-to-american`)
+          .ba(`I spent the bank holiday at the funfair.`)
           .replace(re, ""),
-        ``
+        `I spent the public holiday at the carnival.`
       );
     });
     test(`I had a bicky then went to the chippy.`, () => {
       assert.equal(
-        translator
-          .ab(`I had a bicky then went to the chippy.`, `british-to-american`)
-          .replace(re, ""),
-        ``
+        translator.ba(`I had a bicky then went to the chippy.`).replace(re, ""),
+        `I had a cookie then went to the fish-and-fish-and-chip shop.`
       );
     });
     test(`I've just got bits and bobs in my bum bag.`, () => {
       assert.equal(
         translator
-          .ab(
+          .ba(
             `I've just got bits and bobs in my bum bag.`,
             `british-to-american`
           )
           .replace(re, ""),
-        ``
+        `I've just got odds and ends in my fanny pack.`
       );
     });
     test(`The car boot sale at Boxted Airfield was called off.`, () => {
       assert.equal(
         translator
-          .ab(
+          .ba(
             `The car boot sale at Boxted Airfield was called off.`,
             `british-to-american`
           )
           .replace(re, ""),
-        ``
+        `The swap meet at Boxted Airfield was called off.`
       );
     });
     test(`Have you met Mrs Kalyani?`, () => {
       assert.equal(
-        translator
-          .ab(`Have you met Mrs Kalyani?`, `british-to-american`)
-          .replace(re, ""),
-        ``
+        translator.ba(`Have you met Mrs Kalyani?`).replace(re, ""),
+        `Have you met Mrs. Kalyani?`
       );
     });
     test(`Prof Joyner of King's College, London.`, () => {
       assert.equal(
         translator
-          .ab(`Prof Joyner of King's College, London.`, `british-to-american`)
+          .ba(`Prof Joyner of King's College, London.`, `british-to-american`)
           .replace(re, ""),
-        ``
+        `Prof. Joyner of King's College, London.`
       );
     });
     test(`Tea time is usually around 4 or 4.30.`, () => {
       assert.equal(
-        translator
-          .ab(`Tea time is usually around 4 or 4.30.`, `british-to-american`)
-          .replace(re, ""),
-        ``
+        translator.ba(`Tea time is usually around 4 or 4.30.`).replace(re, ""),
+        `Tea time is usually around 4 or 4:30.`
+      );
+    });
+  });
+  suite(`Highlight translation`, () => {
+    test(`Mangoes are my favorite fruit.`, () => {
+      assert.include(
+        translator.ab(`Mangoes are my favorite fruit.`),
+        `<span class="highlight">favourite</span>`
+      );
+    });
+    test(`I ate yogurt for breakfast.`, () => {
+      assert.include(
+        translator.ab(`I ate yogurt for breakfast.`),
+        `<span class="highlight">yoghurt</span>`
+      );
+    });
+    test(`We watched the footie match for a while.`, () => {
+      assert.include(
+        translator.ba(`We watched the footie match for a while.`),
+        `<span class="highlight">soccer</span>`
+      );
+    });
+    test(`Paracetamol takes up to an hour to work.`, () => {
+      assert.include(
+        translator.ba(`Paracetamol takes up to an hour to work.`),
+        `<span class="highlight">Tylenol</span>`
       );
     });
   });
