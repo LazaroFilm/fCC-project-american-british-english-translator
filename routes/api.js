dc46db48ef14a92b3d060364ee131fa70b280299
Victor Lazaro <lazarofilm@gmail.com>
2021-01-02 16:48:35 -0500

    added error throws

--- routes/api.js
+++ routes/api.js
@@ -6,16 +6,24 @@ module.exports = function (app) {
   const translator = new Translator();
 
   app.route("/api/translate").post((req, res) => {
-    // console.log("_____POST/translate_____");
-    // console.log(req.body);
+    console.log("_____POST/translate_____");
+    console.log(req.body);
     const text = req.body.text;
     const locale = req.body.locale;
-
-    const translation =
-      locale == "american-to-british"
-        ? translator.ab(text)
-        : translator.ba(text);
-    // console.log("route:", translation);
-    res.json({ text, translation });
+    try {
+      if (!text || !locale) throw `Required field(s) missing`;
+      if (text === "") throw `No text to translate`;
+      if ((locale != "british-to-american") & (locale != "american-to-british"))
+        throw `Invalid value for locale field`;
+      const translation =
+        locale == "american-to-british"
+          ? translator.ab(text)
+          : translator.ba(text);
+      console.log("translation:", translation);
+      res.json({ text, translation });
+    } catch (error) {
+      console.log("error:", error);
+      res.json(error);
+    }
   });
 };
